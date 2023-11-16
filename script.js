b11a83ac8817c03e1fcc8a34ca200f47c527c942
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-11-17 02:29:06 +0530

    added-fallow-fb-page-updated

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
-    el.addEventListener('shown.bs.tab', () => {
-      const target = el.getAttribute('data-bs-target')
-      const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
-      bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
-    })
-  })
