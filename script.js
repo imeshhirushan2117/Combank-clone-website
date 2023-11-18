c86c12e2873966a755d2ec02c059fb44dafae8c8
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-11-18 23:12:11 +0530

    key-evevnt-call-img

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const mouseOver = () => {
+    document.getElementById('hover-img').src = 'assets/img/chat-c-2.png'
+}
+
+const mouseOut = () => {
+    document.getElementById('hover-img').src = 'assets/img/chat-c-1.png'
+}
