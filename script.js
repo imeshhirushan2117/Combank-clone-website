49935e8a7e5ed4d0f716f2ed0ffccdbb46744282
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-11-20 12:00:34 +0530

    updated-credit-cad-secction

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ const mouseOver = () => {
 const mouseOut = () => {
     document.getElementById('hover-img').src = 'assets/img/chat-c-1.png'
 }
+
+
+
+
 const iconClick = () => {
-    document.getElementById()
+    var icon = document.getElementById('stataic-img');
+
+    if(icon.style.display == 'none'){
+        icon.style.display = 'block'
+    }else{
+        icon.style.display = 'none'
+    }
+
+    
+
 }
