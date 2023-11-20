3997462ebde99e63c458b2d0a6bacf95b8a7fe60
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-11-20 15:04:23 +0530

    compled-clone-commercial-bank-site

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ const mouseOut = () => {
     document.getElementById('hover-img').src = 'assets/img/chat-c-1.png'
 }
 
-
-
-
 const iconClick = () => {
     var icon = document.getElementById('stataic-img');
 
@@ -20,8 +17,37 @@ const iconClick = () => {
         icon.style.display = 'block'
     }else{
         icon.style.display = 'none'
-    }
-
-    
+    } 
 
 }
+
+const over1 = () => {
+//   var nav =  document.getElementById('overOne').style.display = "block"
+//     console.log("over1")
+
+    
+//     if(nav.style.display == 'none'){
+//         nav.style.display = 'block'
+//     }else{
+//         nav.style.display = 'none'
+//     } 
+
+
+    var nav = document.getElementById('overOne');
+
+    if (nav.style.display === 'none' || nav.style.display === '') {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+
+    console.log("over1");
+
+
+}
+
+ 
+
+
+
+
